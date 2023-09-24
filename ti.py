e311a211c1ee6c0cec4e6fcd156f39feb8928889
Camilo <jc_bernal2011@hotmail.com>
2023-09-24 13:07:11 -0500

    add history

---
+++ ti.py
@@ -0,0 +1,3 @@
+history = []
+history.append("Llegaste cansad@ del trabajo, y solo quieres descansar, prendes la televisión y ves que están presentando algo sobre zombies, ¿qué haces?")
+print(history[0])
