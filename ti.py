cf85e3c027cce450499ee858645c9194ce5c6b11
Camilo <jc_bernal2011@hotmail.com>
2023-09-25 14:19:42 -0500

    add history
    full story added

--- ti.py
+++ ti.py
@@ -1,3 +1,97 @@
 history = []
+#0
 history.append("Llegaste cansad@ del trabajo, y solo quieres descansar, prendes la televisión y ves que están presentando algo sobre zombies, ¿qué haces?")
-print(history[0])
+#1
+history.append("Empiezas a llamar a todos tus conocidos para verificar si lo que esta pasando es real ")
+#2
+history.append("Te das cuenta que lo que esta pasando es real por lo que vas al supermercado a tomar proviciones ")
+#3
+history.append("Sales de tu casa con un bate de baisbol ")
+#4
+history.append("En el camino te encuentras un zombie, decides esconderte ")
+#5
+history.append("Hiciste bien, solo alcanzabas a ver un zombie pero detrás de el habia una docena, luego de pasar los zombies, decides entrar al supermercado ")
+#6
+history.append("Adentro se encuentra solo un zombie, por lo que intentas matarlo para poder tener proviciones ")
+#7
+history.append("Lo lograste matar, mientras veías alimentos para llevarte a casa, encontraste una radio ")
+#8
+history.append("Empiezas a escuchar a un grupo hablar por la radio, asi que decides pedir ayuda  ")
+#9
+history.append("El grupo escucho tu ayuda asi que fueron a rescatarte, ahora haces parte de un grupo TE SALVASTE ")
+#10
+history.append("Empiezas a escuchar a un grupo hablar por la radio, pero decides ignorarlos porque podria ser una trampa  ")
+#11
+history.append("Los zombies que estaban en la zona eran muchos, por lo que pudieron destruir la puerta y entrar ESTAS MUERTO ")
+#12
+history.append("Lo lograste matar, mientras veías alimentos para llevarte a casa, encontraste tu comida favorita ")
+#13
+history.append("Decides abrirla para comer, ya que piensas que puede ser tu ultima comida ")
+#14
+history.append("Si fue tu ultima comida, al abrir la comida hiciste ruido, alertando a los zombies a tu alrededor ESTAS MUERTO ")
+#15
+history.append("Decides guardarla para una ocasión especial")
+#16
+history.append("Un grupo de personas pasaron matando zombies justo afuera de donde estas, decides esconderte ")
+#17
+history.append("Un grupo de personas pasaron matando zombies justo afuera de donde estas, decides pedir ayuda ")
+#18
+history.append("Adentro se encuentra solo un zombie, por lo que decides devolverte a casa ")
+#19
+history.append("Al devolverte a casa te encuentras con que tu vecino necesita ayuda, decides acompañarlo")
+#20
+history.append("El tenia comida guardada para mucho tiempo, por lo que te quedas Viviendo con el, TE SALVASTE")
+#21
+history.append("Al devolverte a casa te encuentras con que tu vecino necesita ayuda, decides ignorarlo ")
+#22
+history.append("Alcanzaste a regresar  a tu casa, pero te das cuenta que tus proviciones no duraran mas de una semana, MORISTE ")
+#23
+history.append("Hiciste bien, solo alcanzabas a ver un zombie pero detrás de el habia una docena, luego de pasar los zombies, decides devolverte a tu casa ")
+#24
+history.append("Al volver a casa, decides advertir a todos tus vecinos de lo que esta pasando")
+#25
+history.append("Logran crear una comunidad exitosa, y autosustentable ")
+#26
+history.append("Al volver a casa, decides intentar robar a uno de tus vecinos que esta en avanzada edad")
+#27
+history.append("No conocías la historia de tu vecino, el era parte de las fuerzas armadas, por lo que apenas entraste te disparo, MORISTE ")
+#28
+history.append("Logras acabar con el, pero habían muchos mas zombies de donde salio este, sales corriendo ")
+#29
+history.append("Te perdiste en un túnel subterráneo de la ciudad, jamás lograste salir, MORISTE ")
+#30
+history.append("En el camino te encuentras un zombie, decides atacarlo ")
+#31
+history.append("Logras acabar con el, pero habían muchos mas zombies de donde salio este, intentas acabar con todos ")
+#32
+history.append("Decides colgarte para no ser un problema para nadie, MORISTE ")
+#33
+history.append("Lograste acabsr con todos pero uno de ellos te alcanzo a morder, por lo que decides cortarte la pierna")
+#34
+history.append("No pudiste detener la hemorragia, ESTAS MUERTO ")
+#35
+history.append("Sales de tu casa con una pistola ")
+#36
+history.append("En el camino te encuentras un zombie, decides esconderte ")
+#37
+history.append("Al intentar esconderte te encuentras con grupo, y al ver el arma te disparan primero, ESTAS MUERTO ")
+#38
+history.append("En el camino te encuentras un zombie, decides atacarlo ")
+#39
+history.append("Hiciste much ruido, alertaste a los zombies, ESTAS MUERTO ")
+#40
+history.append("Te quedas sentado pensando que es una pelicula")
+#41
+history.append("Decides pedir una pizza por domicilio como cena ")
+#42
+history.append("Llego la pizza, abriste la puerta para recibirla, y resulto ser un zombie ESTAS MUERTO  ")
+#43
+history.append("Se te hace raro que la Pelicula siga siendo mas una noticia que entrentenimiento ")
+#44
+history.append("Lo ignoras y empiezas a preparar tu cena, escuchas un ruido afuera ")
+#45
+history.append("Decides tomar precaucion, pero aun asi los zombies te logran atrapar, ESTAS MUERTO   ")
+#46
+history.append("Ellos fuera matar los zombies, tambien estaban quemando todo para evitar que el virus se propagara, MORISTE QUEMADO ")
+#47
+history.append("Ellos escucharon tu llamado y se dieron cuenta que eres una buena persona ya que compartiste tu comida favorita con ellos, TE SALVASTE, AHORA PERTENECES A UN GRUPO ")
